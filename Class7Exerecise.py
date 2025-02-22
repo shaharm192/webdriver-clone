1fc035eb0ea359231d0921cd20c4a614452c0de0
shaharm192 <shahar.m192@gmail.com>
2025-02-22 21:00:28 +0200

    updated Class7Exerecise.py in main

--- Class7Exerecise.py
+++ Class7Exerecise.py
@@ -1,10 +1,1 @@
-import datetime
-
-def print_name_and_date(name):
-    current_time = datetime.datetime.now()
-    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Name: {name}")
-    print(f"Printed on: {formatted_time}")
-
-# Replace 'Your Name' with your actual name
-print_name_and_date("shahar moshin")
+print('lets try to conflict')
